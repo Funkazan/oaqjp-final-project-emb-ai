dd6c9f062ad229256a01d52aff744a507df634f5
Funkazan <akkan.volkan@gmail.com>
2026-02-03 21:48:49 -0500

    finished

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module initiates the Flask application for emotion detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +8,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Analyzes the text provided by the user and returns the emotion scores.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -15,12 +21,17 @@ def sent_analyzer():
         f"For the given statement, the system response is"
         f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
         f"'fear': {response['fear']}, 'joy': {response['joy']} and "
-        f"'sadness': {response['sadness']} The dominant emotion is **{response['dominant_emotion']}**"
+        f"'sadness': {response['sadness']}, "
+        f"The dominant emotion is **{response['dominant_emotion']}**"
     )
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the main application page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
